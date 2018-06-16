d975f58707c3f6b0f02b88cdcf7a1f47d4b6602e
Aj <adtune6509@student.waynecc.edu>
2018-06-16 07:39:28 -0400

    Update client-online.js

--- client-online.js
+++ client-online.js
@@ -1,28 +1,28 @@
 /*: 
-*
-* MultiLobby
-*
-* @plugindesc Online functions
-* @author Yuuta Kirishima / Dexter / Persia Indie Studios
-*
-* @param host
-* @desc server URL
-* @default http://uniforce.herokuapp.com
-*
-*
-*/
+ *
+ * MultiLobby
+ *
+ * @plugindesc Online functions
+ * @author Yuuta Kirishima / Dexter / Persia Indie Studios
+ *
+ * @param host
+ * @desc server URL
+ * @default http://uniforce.herokuapp.com
+ *
+ *
+ */
 var parameters = PluginManager.parameters("MultiLobby");
 var HOST = String(parameters['host']);
 var MultiLobby = MultiLobby || {};
 (function() {
 
     MultiLobby.socket = io.connect(HOST);
-    MultiLobby.socket.on('connect_error', function(){
+    MultiLobby.socket.on('connect_error', function() {
         throw new Error('Could not connect to Uniforce servers, please try again later.');
     })
     MultiLobby.socket.emit('player-connected');
     var online_players = 0;
-    MultiLobby.socket.on('online-players', function(val){
+    MultiLobby.socket.on('online-players', function(val) {
         online_players = val;
     })
     MultiLobby.getPlayersOnline = function() {
@@ -43,15 +43,15 @@ var MultiLobby = MultiLobby || {};
     }
 
     MultiLobby.chat.Window.prototype = Object.create(Window_Base.prototype);
-    MultiLobby.chat.Window.prototype.constructor = MultiLobby.chat.Window.prototype.initialize = function(x, y){
+    MultiLobby.chat.Window.prototype.constructor = MultiLobby.chat.Window.prototype.initialize = function(x, y) {
         Window_Base.prototype.initialize.call(this, x, y, this.windowWidth(), this.windowHeight());
         this.refresh();
     }
     var __Scene_Map_start = Scene_Map.prototype.start;
     Scene_Map.prototype.start = function() {
-            __Scene_Map_start.call(this);
+        __Scene_Map_start.call(this);
         console.log(this.chatWindow);
-        if(this.chatWindow == undefined || null){
+        if (this.chatWindow == undefined || null) {
             this.chatWindow = new MultiLobby.chat.Window(0, 425);
             this.addWindow(this.chatWindow);
         } else {
@@ -59,24 +59,24 @@ var MultiLobby = MultiLobby || {};
         }
 
     };
-    MultiLobby.socket.on('got-new-message', function(message){
+    MultiLobby.socket.on('got-new-message', function(message) {
         MultiLobby.chat.message = message;
         MultiLobby.chat.newMessage = true;
     })
     var _Scene_Map_Update = Scene_Map.prototype.update;
     Scene_Map.prototype.update = function() {
         _Scene_Map_Update.call(this)
-       var theWindow = this.chatWindow;
-       if(MultiLobby.chat.newMessage){
-            theWindow.refresh(GameJolt.getUserName() +': ' + MultiLobby.chat.message);
+        var theWindow = this.chatWindow;
+        if (MultiLobby.chat.newMessage) {
+            theWindow.refresh(GameJolt.getUserName() + ': ' + MultiLobby.chat.message);
             MultiLobby.chat.newMessage = false;
             MultiLobby.chat.messageCount++
-            MultiLobby.chat.startY = MultiLobby.chat.startY + 25;
-       }
+                MultiLobby.chat.startY = MultiLobby.chat.startY + 25;
+        }
     };
 
     MultiLobby.chat.Window.prototype.refresh = function(chatMessage) {
-        if(MultiLobby.chat.messageCount === 7){
+        if (MultiLobby.chat.messageCount === 7) {
             this.contents.clear();
             MultiLobby.chat.messageCount = 0;
             MultiLobby.chat.startY = 0;
@@ -103,33 +103,20 @@ var MultiLobby = MultiLobby || {};
     //
     */
     MultiLobby.dailyQuest = MultiLobby.dailyQuest || {};
-    MultiLobby.dailyQuest.recievedBonus = function() {
-        MultiLobby.socket.emit('recieved-bonus?');
-    }
-    MultiLobby.socket.on('bonus-recieved-2', function(bool){
-        if(bool){
+    MultiLobby.dailyQuest.start = function() {
+        var dailydata = {
+            username: GameJolt.getUserName(),
 
-        } else {
-            console.log('mission expired while not online');
+            time: new Date.getTime()
+        }
+        MultiLobby.socket.emit('daily-start', dailydata);
+    }
+
+    MultiLobby.socket.on('daily-check', function() {
+        var dailydata = {
+            username: GameJolt.getUserName(),
+
+            time: new Date.getTime()
         }
     })
-    MultiLobby.dailyQuest.expire = function(){
-        MultiLobby.socket.emit('check-expire');
-    }
-    MultiLobby.socket.on('mission-expired', function(bool){
-        if(bool){
-            console.log('Mission expired.')
-            Galv.QUEST.status(id)
-            MultiLobby.socket.emit('bonus-recieved');
-        }
-    })
-    setInterval(function() {
-        MultiLobby.dailyQuest.expire();
-      }, 5000);
-
-      var ___Scene_Map_start = Scene_Map.prototype.start;
-      Scene_Map.prototype.start = function() {
-        ___Scene_Map_start.call(this);
-        MultiLobby.dailyQuest.recievedBonus();
-    };
 })();
